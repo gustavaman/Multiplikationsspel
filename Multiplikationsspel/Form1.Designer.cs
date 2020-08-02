730e86e652157a76caa04b0b4454a39e85c86780
Gustav Åman <gustav.aman1@gmail.com>
2020-08-03 01:17:40 +0200

    Fungerar men är inte snyggt.

---
+++ Multiplikationsspel/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System.Windows.Forms;
+
+namespace Multiplikationsspel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.startrandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.display.AutoSize = true;
+            this.display.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.Location = new System.Drawing.Point(12, 34);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(731, 95);
+            this.display.TabIndex = 0;
+            this.display.Text = "Multiplikationsspel";
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(76, 331);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(300, 76);
+            this.start.TabIndex = 1;
+            this.start.Text = "Spela i ordning";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.Location = new System.Drawing.Point(136, 209);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(137, 58);
+            this.timer.TabIndex = 3;
+            this.timer.Text = "timer";
+            // 
+            // answer
+            // 
+            this.answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answer.Location = new System.Drawing.Point(279, 188);
+            this.answer.Name = "answer";
+            this.answer.ReadOnly = true;
+            this.answer.Size = new System.Drawing.Size(204, 102);
+            this.answer.TabIndex = 6;
+            // 
+            // startrandom
+            // 
+            this.startrandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startrandom.Location = new System.Drawing.Point(382, 331);
+            this.startrandom.Name = "startrandom";
+            this.startrandom.Size = new System.Drawing.Size(286, 76);
+            this.startrandom.TabIndex = 7;
+            this.startrandom.Text = "Spela i slumpmässig ordning";
+            this.startrandom.UseVisualStyleBackColor = true;
+            this.startrandom.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 450);
+            this.Controls.Add(this.startrandom);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.display);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label timer;
+        private TextBox answer;
+        private Button startrandom;
+    }
+}
+
---
+++ Multiplikationsspel/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplikationsspel
+{
+    public partial class Form1 : Form
+    {
+        bool random;
+        Stopwatch time = new Stopwatch();
+        Random rnd = new Random();
+        List<string> multtable = new List<string>();
+        public string MultTableGenerator(int a)
+        {
+            List<string> multtable = new List<string>();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int p = 0; p <= 10; p++)
+                {
+                    if (p >= i)
+                    {
+                        multtable.Add(i + "·" + p);
+                    }
+                }
+            }
+
+            return multtable[a];
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            answer.Hide();
+
+            timer.Hide();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void start_Click(object sender, EventArgs e)
+        {
+            if (sender == start)
+                random = false;
+            else
+                random = true;
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int p = 0; p <= 10; p++)
+                {
+                    if (p >= i)
+                    {
+                        multtable.Add(i + "·" + p);
+                    }
+                }
+            }
+
+            timer.Hide();
+            start.Hide();
+            startrandom.Hide();
+            answer.Show();
+            if (!random)
+                display.Text = multtable[0];
+            if (random)
+                display.Text = multtable[rnd.Next(0, multtable.Count)];
+            time.Reset();
+            time.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back && answer.Text.Length > 0)
+                answer.Text = answer.Text.Remove(answer.Text.Length - 1);
+            else if (e.KeyCode == Keys.D1 || e.KeyCode == Keys.D2 || e.KeyCode == Keys.D3 || e.KeyCode == Keys.D4 || e.KeyCode == Keys.D5 || e.KeyCode == Keys.D6 || e.KeyCode == Keys.D7 || e.KeyCode == Keys.D8 || e.KeyCode == Keys.D9 || e.KeyCode == Keys.D0)
+                answer.Text += (char)e.KeyCode;
+            if (display.Text.Contains("·") && answer.Text == (int.Parse(display.Text.Split('·')[0]) * int.Parse(display.Text.Split('·')[1])).ToString() && multtable.Count != 1)
+            {
+                multtable.Remove(display.Text);
+                if (!random)
+                    display.Text = multtable[0];
+                if (random)
+                    display.Text = multtable[rnd.Next(0, multtable.Count)];
+                answer.Text = "";
+            }
+            else if (display.Text.Contains("·") && answer.Text == (int.Parse(display.Text.Split('·')[0]) * int.Parse(display.Text.Split('·')[1])).ToString() && multtable.Count == 1)
+            {
+                time.Stop();
+                display.Text = "";
+                answer.Hide();
+                start.Show();
+                startrandom.Show();
+                timer.Show();
+                timer.Text = (time.ElapsedMilliseconds / 1000).ToString() + " sekunder.";
+            }
+        }
+    }
+}
